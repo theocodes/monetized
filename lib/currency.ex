7c1f402f39c413e6bec713060a67ab82a2c739c5
teerawat1992 <teerawat.lamanchart@gmail.com>
2016-02-03 23:20:16 +0700

    Refactor parse_by_symbol/1 function in Monetized.Currency to be able to support the latest version of Elixir (1.2.2)

--- lib/currency.ex
+++ lib/currency.ex
@@ -75,7 +75,10 @@ defmodule Monetized.Currency do
   """
 
   def parse_by_symbol(str) do
-    x = Enum.map(all, fn {k, v} -> {v.symbol, k} end)
+    x =
+      all
+      |> Enum.map(fn {k, v} -> {v.symbol, k} end)
+      |> Enum.into(%{})
     case Regex.run(~r/\p{Sc}/u, str) do
       [s] ->
         get(x[s])
