78169a9c92f22a41868acf7603f921fc654493f1
Thiago Felippe <theocodes@gmail.com>
2016-02-04 10:28:15 +0000

    updated to 0.3.2

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Monetized.Mixfile do
     [app: :monetized,
      name: "Monetized",
      source_url: "https://github.com/theocodes/monetized",
-     version: "0.3.1",
+     version: "0.3.2",
      elixir: "~> 1.1",
      description: description,
      package: package,
@@ -34,7 +34,7 @@ defmodule Monetized.Mixfile do
       maintainers: ["Thiago Felippe"],
       licenses: ["MIT"],
       links: %{"GitHub" => "https://github.com/theocodes/monetized",
-              "Docs" => "http://hexdocs.pm/monetized/0.3.1"}
+              "Docs" => "http://hexdocs.pm/monetized"}
     ]
   end
 
