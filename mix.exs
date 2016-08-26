1e9633d5d117b7bbb3460aca3c2525408c9d08e8
Nick Auger <nick@revzilla.com>
2016-08-26 15:20:39 -0400

    bump the ecto requirement to match core app

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule Monetized.Mixfile do
       {:earmark, "~> 0.2.1",  only: :dev},
       {:inch_ex, "~> 0.5.1",  only: :docs},
       {:decimal, "~> 1.1.2"},
-      {:ecto,    "~> 1.1.7"}
+      {:ecto,    "~> 2.0.4"}
     ]
   end
 
